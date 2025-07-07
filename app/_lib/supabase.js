2ba632c797fccef24e7b7f3ac1b90de4d880b659
Punyansh Singla <167777471+7425036696@users.noreply.github.com>
2025-07-07 20:29:37 +0530

    Update supabase.js

--- app/_lib/supabase.js
+++ app/_lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+"https://kqhpimjtfpbiqxqhhwik.supabase.co",
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtxaHBpbWp0ZnBiaXF4cWhod2lrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE4OTIzMTQsImV4cCI6MjA2NzQ2ODMxNH0.wOCHpI4dHbUuVqpkdr6i3F1nCwhJSR4acofkJRoLlr4"
 );
